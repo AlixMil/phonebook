82e6cd824c06a54a940e6c175608f54c37fd247a
AlixMil <jojmusor@gmail.com>
2019-08-02 21:25:19 +0300

    Perfect View

--- src/Components/InsertContact.js
+++ src/Components/InsertContact.js
@@ -1,11 +1,28 @@
 import React from 'react';
-import { Form } from 'react-bootstrap';
+import { Form, Row, Col, Button } from 'react-bootstrap';
 import '../App.css'
 
 export default function InsertContact(props) {
   return (
     <Form>
-      <Form.Control type="contact" placeholder="name contact"/>
+      
+        <Form.Group>
+          <Row>
+            <Col>
+              <Form.Control type="contactName" placeholder="name contact"/>
+            </Col>
+            <Col>
+              <Form.Control type="contactSurName" placeholder="sur name contact"/>
+            </Col>
+            <Col>
+             <Form.Control type="contactNumber" placeholder="number contact"/>
+            </Col>
+            <Col>
+              <Button>Add</Button>
+            </Col>
+          </Row>
+        </Form.Group>
+      
     </Form>
   )
 }
